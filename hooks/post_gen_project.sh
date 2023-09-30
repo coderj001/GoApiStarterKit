188f5da99efccae2cfb0bc09ea7753e899a2f9cd
Raju Ghorai <amirajubolchi001@gmail.com>
2023-09-30 20:26:23 +0530

    😙added: dependabot, docker (dev and prod) and rest.http files

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 IFS=$'\n\t'
 
 #
@@ -15,10 +18,50 @@ function echo_info() {
 # Step 1:
 #
 
+if [[ echo "{{cookiecutter.use_docker}}" | grep -iq "^n" ]]; then
+	rm -rf docker/ compose/
+fi
+
+#
+# Step 2:
+#
+
+if [[ echo "{{cookiecutter.use_rest_http}}" | grep -iq "^n" ]]; then
+	rm -rf rest.txt
+elif
+	echo_info "Install rest.http extension for vscode"
+	mv rest.txt rest.http
+fi
+
+#
+# Step 3:
+#
+
+if [[ echo "{{cookiecutter.use_github_action}}" | grep -iq "^n" ]]; then
+	rm -rf .github/
+fi
+
+#
+# Step 4:
+#
+
+echo_info "Setup env"
+cp env.example
+cp env.example .env
+echo """
+
+# JWT Secret
+JWT_SECRET=$(openssl rand -base64 32)
+""" >> .env
+
+#
+# Step 5:
+#
+
 echo_info "Initializing Git"
 git init
 git add .
-git commit -n -m "Initial Commit"
+git commit -n -m "🔧 chore: initial commit"
 
 #
 # Done:
--- {{cookiecutter.project_name}}/README.md
+++ {{cookiecutter.project_name}}/README.md
@@ -1,2 +1,5 @@
 # {{ cookiecutter.project_name|replace('go', '')|upper|replace('-',' ') }}
 
+** {{cookiecutter.project_long_description}} **
+
+
