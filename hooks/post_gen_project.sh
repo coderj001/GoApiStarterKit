30c70add1158d4f0cbfb25567083bf26aa7d9d20
Raju Ghorai <amirajubolchi001@gmail.com>
2023-10-02 10:56:39 +0530

     Added: docker config and make files

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -29,66 +29,6 @@ fi
 if [[ echo "{{cookiecutter.use_rest_http}}" | grep -iq "^y" ]]; then
 	echo_info "Install rest.http extension for vscode"
 	touch rest.http
-	echo """
-@baseURL = http://localhost:8000
-@token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2ODg1NzgwMzQsImlhdCI6MTY4ODQ5MTYzNH0.03hUcVbQUOn6MX-FoWFcThvPF9PBgozJIhYWc0V9E6Q
-
-### Test API
-GET {{baseURL}}/health
-Content-Type: application/json
-
-### User Register
-POST {{baseURL}}/user/register
-Content-Type: application/json
-
-{
-  "name": "John Doe",
-  "phone": "1234567890",
-  "email": "johndoe@example.com",
-  "password": "password123"
-}
-
-### User Login
-POST {{baseURL}}/user/login
-Content-Type: application/json
-
-{
-  "email": "johndoe@example.com",
-  "password": "password123"
-}
-
-### Create Contact
-POST {{baseURL}}/contact
-Content-Type: application/json
-Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2ODg4MDA0MTMsImlhdCI6MTY4ODcxNDAxM30.nJhkx4n7VeoyQQ0hax5hsuYFG0Yo6Q4OIN3_cFqIGUw
-
-{
-  "name": "neo ai",
-  "phone_number": "1234567891"
-}
-
-### Mark as spam
-POST {{baseURL}}/spam
-Content-Type: application/json
-
-{
-  "phone_number": "1234567890"
-}
-
-### Search by Name
-GET {{baseURL}}/search?name=neo
-Content-Type: application/json
-Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2ODg4MDA0MTMsImlhdCI6MTY4ODcxNDAxM30.nJhkx4n7VeoyQQ0hax5hsuYFG0Yo6Q4OIN3_cFqIGUw
-
-### Search by Phone Number
-GET {{baseURL}}/search?phone_number=1234567890
-Content-Type: application/json
-Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2ODg4MDA0MTMsImlhdCI6MTY4ODcxNDAxM30.nJhkx4n7VeoyQQ0hax5hsuYFG0Yo6Q4OIN3_cFqIGUw
-
-### User Details
-GET {{baseURL}}/user/1
-Content-Type: application/json
-	""">rest.http
 fi
 
 #
